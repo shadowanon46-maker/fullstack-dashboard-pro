36a94dbdaa82ac8cd57098e88792be02d3141753
Fitrah Andre <shadowanon46@gmail.com>
2025-12-25 00:51:36 +0700

    feat: setup Next.js 15.5.7, Tailwind CSS v4, and professional folder structure

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+  export default function Home() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Dashboard Profesional - Hari 2 Selesai!</h1>
+      </div>
+    )
+  }
